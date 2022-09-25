e3ab940d0df6eac504c864fe7c74c7d0e2c77cad
seyoungjoy <diddpwl80@gmail.com>
2022-09-25 22:34:33 +0900

    :recycle: Refactor js-algorithm 04-01

--- section04/pb01.js
+++ section04/pb01.js
@@ -1,6 +1,7 @@
 // 자릿수의 합
 // 각 자릿수의 합이 최대인 값 출력
 // 이때 중복값일경우 원래 큰 숫자를 출력
+// 원본 요소를 반환해야하기때문에 max일때 따로 그 값을 저장하는게 포인트!
 
 function solution(arr){
     const leng = arr.length;
@@ -13,6 +14,7 @@ function solution(arr){
             answer = arr[i];
         } else if(sum === max){
             if(answer < arr[i]) answer = arr[i];
+            //2) answer = Math.max(answer,arr[i]);
         }
     }
     return answer;
