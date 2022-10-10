50c2cdde9824da6c2e82a394e702d9b1fe0f53fa
seyoungjoy <diddpwl80@gmail.com>
2022-10-10 14:28:55 +0900

    :sparkles: section 5-8

---
+++ section05/pb08.js
@@ -0,0 +1,19 @@
+// 모든 아나그램 찾기
+// a 문자열에서 아나그램 b를 가지는 경우의 수를 찾으시오.
+// 부분 문자열은 연속된 문자열!!!
+function compareMaps(map1, map2){
+    // map1과 map2 size 비교
+    // key, value값 비교
+}
+function solution(s, t){
+    // b에 대한 hash map 생성
+    // length = 2 인 a hash map 생성
+    // 슬라이딩 윈도우로 a의 값을 3개씩 묶어 hasp map으로 만들어 비교
+    // 값이 0일 때는 값을 delete 해야함.
+    // lt제거 rt 추가,
+
+}
+
+let a="bacaAacba";
+let b="abc";
+console.log(solution(a, b));
